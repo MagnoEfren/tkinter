26d2792f9e732d88484a622c217d4b8d8d8db8b1
MagnoEfren <58579707+MagnoEfren@users.noreply.github.com>
2022-10-28 19:19:46 -0500

    Update main.py

--- Efecto Matrix/main.py
+++ Efecto Matrix/main.py
@@ -39,7 +39,6 @@ class Matrix(Frame):
 				self.obj = self.canvas.create_text(20+x, -200+y+j, text= choice(self.caracteres),
 					fill = 'green2', font= ('Arial', 14))
 				self.letters.append(self.obj)
-
 	def update(self):
 		for letter in self.letters:
 			v = choice(self.velocidad)
@@ -51,13 +50,11 @@ class Matrix(Frame):
 
 			if self.green >=250:
 				self.green = 0
-
 		if y[1] >=800:
 			self.draw()
 			if y[1]>= 1200:
 				self.letters.clear()
-				self.canvas.delete('all')
-		
+				self.canvas.delete('all')		
 		self.canvas.after(80, self.update)
 
 if __name__ == '__main__':
