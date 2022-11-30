b976224818553e0e670283b6efd874a2a9d23a42
MagnoEfren <58579707+MagnoEfren@users.noreply.github.com>
2022-11-30 00:13:28 -0500

    Update Snake.py

--- Juego Snake/Snake.py
+++ Juego Snake/Snake.py
@@ -1,7 +1,6 @@
 # Juego Snake
 # @autor: Magno Efren
 # Youtube: https://www.youtube.com/c/MagnoEfren
-
 #print('🟢❤🍎🌍💛💚🧡🧡❤💥⚪🟥🟧🟨🟩🟦🟫🟪🔶🔸🔷🔹🔺🔻')
 
 from tkinter import  Tk, Frame, Canvas, Button,Label, IntVar, ALL
@@ -15,13 +14,10 @@ posicion_y = 15
 posicion_food = (15,15)
 posicion_snake = [(75,75)]
 nueva_posicion =[(15,15)]
-
 mixer.init()
-
 
 def coordenadas_snake():
     global direccion, posicion_snake,x,y ,nueva_posicion 
-
     if direction =='up': # arriba
         y =  y-30
         nueva_posicion[0:] = [(x, y)]
@@ -50,15 +46,12 @@ def coordenadas_snake():
             x=15
         elif x <=0:
             x=15
-
     posicion_snake = nueva_posicion + posicion_snake[:-1]
-
     for parte, lugar in zip(canvas.find_withtag("snake"), posicion_snake):
         canvas.coords(parte, lugar)
 
 def direccion(event):
     global direction
-
     if event == 'left':
         if direction != 'right':
             direction = event
@@ -71,31 +64,22 @@ def direccion(event):
     elif event == 'down':
         if direction != 'up':
             direction = event
-
 def movimiento():
     global posicion_food, posicion_snake,nueva_posicion
     posiciones = [15, 45, 75,105,135,165, 195, 225, 255, 
     285, 315, 345, 375, 405, 435, 465] 
-
     coordenadas_snake()
-
     if posicion_food == posicion_snake[0]:
         n = len(posicion_snake)
-
         cantidad['text'] = 'Cantidad 🍎 : {}'.format(n)
-
         posicion_food = (random.choice(posiciones), random.choice(posiciones))
         posicion_snake.append(posicion_snake[-1])
-
         mixer.music.load("audio_snake.mp3")
         mixer.music.play(loops=0)
-
         if posicion_food not in posicion_snake:
             canvas.coords(canvas.find_withtag("food"), posicion_food)
-
         canvas.create_text(*posicion_snake[-1], text= '▀', fill='green2', 
             font = ('Arial',20), tag ='snake')
-
     if posicion_snake[-1] == nueva_posicion[0] and len(posicion_snake)>=4: 
         cruzar_snake()
 
@@ -104,7 +88,6 @@ def movimiento():
         	maximo_nivel()     
 
     cantidad.after(300, movimiento)
-
 
 def cruzar_snake():
     canvas.delete(ALL)
@@ -122,7 +105,6 @@ def salir ():
     ventana.destroy()
     ventana.quit()
 
-
 ventana = Tk()
 ventana.config(bg='black')
 ventana.title('Juego Snake')
@@ -139,14 +121,12 @@ ventana.bind("<KeyPress-Up>", lambda event:direccion('up'))
 ventana.bind("<KeyPress-Down>", lambda event:direccion('down'))
 ventana.bind("<KeyPress-Left>", lambda event:direccion('left'))
 ventana.bind("<KeyPress-Right>",  lambda event:direccion('right'))
-
 canvas = Canvas(frame_2, bg='black', width=479, height=479)
 canvas.pack()
 
 for i in range(0,460,30):
     for j in range(0,460,30):
         canvas.create_rectangle(i,j,i+30, j+30, fill='gray10')
-
 
 canvas.create_text(75,75, text='🍎', fill='red2',
  font = ('Arial',18), tag = 'food')
